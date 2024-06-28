c8000d9b6832b6e23a5cab98de966a5e620b5422
recabet <rajab.iskenderli@gmail.com>
2024-06-29 02:39:28 +0400

    Mostly fixed just needs some finishing touches

--- model.py
+++ model.py
@@ -2,48 +2,65 @@ import numpy as np
 
 
 class LinearRegression:
-	def __init__ (self, rate=0.01, iter=10000):
+	def __init__ (self, rate=0.01, iter=1000):
 		self.rate = rate
 		self.iter = iter
-		self.k = 0
-		self.b = 0
-		self.X_mean = 0
-		self.X_std = 1
+		self.w = None
+		self.b = np.random.randn()
+		self.X_mean = None
+		self.X_std = None
 		self.y_mean = 0
 		self.y_std = 1
 	
-	def fit (self, X, y):
-		n = len(y)
-		
-		self.X_mean = np.mean(X)
-		self.X_std = np.std(X)
+	def __normalize (self, X, y):
+		self.X_mean = np.mean(X, axis=0)
+		self.X_std = np.std(X, axis=0)
 		self.y_mean = np.mean(y)
 		self.y_std = np.std(y)
-		
 		X_normalized = (X - self.X_mean) / self.X_std
 		y_normalized = (y - self.y_mean) / self.y_std
+		return X_normalized, y_normalized
+	
+	def fit (self, X, y):
+		if X.ndim == 1:
+			X = X.reshape(-1, 1)
+		if y.ndim == 2:
+			y = y.flatten()
+		
+		n, m = X.shape
+		self.w = np.random.randn(m)
+		
+		X_normalized, y_normalized = self.__normalize(X, y)
 		
 		for i in range(self.iter):
-			y_pred = self.k * X_normalized + self.b
+			y_pred = np.dot(X_normalized, self.w) + self.b
 			error = y_pred - y_normalized
-			k_gradient = (1 / n) * np.sum(error * X_normalized)
+			w_gradient = (1 / n) * np.dot(X_normalized.T, error)
 			b_gradient = (1 / n) * np.sum(error)
-			self.k -= self.rate * k_gradient
+			self.w -= self.rate * w_gradient
 			self.b -= self.rate * b_gradient
-			
-		self.k = self.k * (self.y_std / self.X_std)
-		self.b = self.b * self.y_std + self.y_mean - self.k * self.X_mean
 		
-		return self.k, self.b
+		self.w = self.w * (self.y_std / self.X_std)
+		self.b = self.b * self.y_std + self.y_mean - np.dot(self.w, self.X_mean)
+		
+		return self.w, self.b
 	
 	def compute_cost (self, X, y):
+		if X.ndim == 1:
+			X = X.reshape(-1, 1)
+		if y.ndim == 2:
+			y = y.flatten()
+		
 		m = len(y)
 		pred = self.predict(X)
 		cost = (1 / (2 * m)) * np.sum((pred - y) ** 2)
 		return cost
 	
 	def predict (self, X):
-		return X * self.k + self.b
+		if X.ndim == 1:
+			X = X.reshape(-1, 1)
+		
+		return np.dot(X, self.w) + self.b
 	
 	def r_squared (self, X, y):
 		y_pred = self.predict(X)
